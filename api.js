f995b25b92702d514136f640e9c09f85c7d4fe50
Kent <kentqlee@gmail.com>
2016-06-27 21:48:04 -0700

    Add Restaurant, Business, and Phone Number for Yelp API Search

--- api.js
+++ api.js
@@ -1,35 +1,63 @@
 // Yelp API starter module in ES6
 
-import Yelp from 'yelp';
-import yelpKeys from './yelpKeys'; // Create a file that includes Yelp keys
+const Yelp = require('yelp');
+const dotenv = require('dotenv');
+// Invoke our env file
+dotenv.config();
 
 // Create a Yelp object, passing in required parameters
 const yelp = new Yelp({
-  consumer_key: yelpKeys.consumer_key,
-  consumer_secret: yelpKeys.consumer_secret,
-  token: yelpKeys.token,
-  token_secret: yelpKeys.token_secret
+  consumer_key: process.env.YELP_CONSUMER_KEY,
+  consumer_secret: process.env.YELP_CONSUMER_SECRET,
+  token: process.env.YELP_TOKEN,
+  token_secret: process.env.YELP_TOKEN_SECRET,
 });
 
 const YelpSearch = {
 
   // Restaurant Search
-  SearchRestaurants: (searchParams, callback) => {
+  Restaurant: (searchParams, callback) => {
     // Pass in search parameters
     yelp.search(searchParams)
       .then(data => {
         if (callback) {
           callback(data);
         }
-        res.send(JSON.stringify(data));
       })
       .catch(err => {
         if (err) {
-          console.err('Error searching Yelp: ', err);
+          console.error('Error searching Yelp Restaurants: ', err);
         }
       });
   },
 
+  // Business Search
+  Business: (searchParams) => {
+    yelp.business(searchParams, callback)
+      .then(data => {
+        if (callback) {
+          callback(data);
+        }
+      })
+      .catch(err => {
+        if (err) {
+          console.error('Error searching Yelp Businesses: ', err);
+        }
+      });
+  },
+
+  // Phone Number Search
+  Phone: (phoneNumber) => {
+    yelp.phoneSearch(phoneNumber, callback)
+      .then(number => {
+        if (callback) {
+          callback(number);
+        }
+      })
+      .catch(err => {
+        console.error('Error searching Yelp Phone Number: ', err);
+      });
+  }
 };
 
-export default YelpSearch;
+module.exports = YelpSearch;
